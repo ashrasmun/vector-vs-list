2764a5b43552f1437a84f1e7ce7ee129d4c08690
burbon <ashrasmun@gmail.com>
2019-06-15 15:35:39 +0200

    Adds stubs for benchmarking functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <list>
+#include <chrono>
+// #include <complex_class>
+
+void benchmark_simple()
+{
+    std::vector<int> simple_vector;
+    std::list<int> simple_list;
+}
+
+void benchmark_complex()
+{
+    std::vector<int> complex_vector;
+    std::list<int> complex_list;
+}
 
 int main()
 {
+    benchmark_simple();
+    benchmark_complex();
+
     std::cout << "Just a test";
     return 0;
 }
