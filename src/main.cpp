306be4d4c6a978bb9d888c218ee5d46afa8442ec
ashrasmun <ashrasmun@gmail.com>
2019-06-29 17:55:23 +0200

    Adds a relatively complex to copy class

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,87 @@
 #include <vector>
 #include <list>
 #include <chrono>
-// #include <complex_class>
+#include <map>
+
+// Complex class definition
+class SomeComplexClass;
+
+class Something
+{
+public:
+    Something(const SomeComplexClass&) {}
+
+private:
+    std::string complexStr;
+    std::map<
+        std::pair<
+            std::string,
+            std::chrono::duration<
+                std::chrono::nanoseconds
+                >
+            >,
+        std::map<
+            int,
+            std::string
+            >
+        > justSomeWeirdMap;
+};
+
+class Base
+{
+public:
+    virtual ~Base() {}
+    virtual void someVirtualMethod() {}
+    virtual void someVirtualMethod1() {}
+    virtual void someVirtualMethod2() {}
+    virtual void someVirtualMethod3() {}
+    virtual void someVirtualMethod4() {}
+    virtual void someVirtualMethod5() {}
+    virtual void someVirtualMethod6() {}
+    virtual void someVirtualMethod7() {}
+    virtual void someVirtualMethod8() {}
+    virtual void someVirtualMethod9() {}
+    virtual void someVirtualMethod10() {}
+    virtual void someVirtualMethod11() {}
+
+    static void someStaticMethod() {}
+
+    static constexpr int someConstexprInt = 0;
+};
+
+class SomeComplexClass
+    : public Base
+{
+    friend class Something;
+
+public:
+    virtual void someVirtualMethod() {}
+
+private:
+    void somePrivateMethod(int test1, char test2)
+    {
+        test1 = static_cast<int>(test2);
+        intMember += test1;
+        strMember = std::to_string(intMember);
+    }
+    virtual void someVirtualMethod1() {}
+    virtual void someVirtualMethod2() {}
+    virtual void someVirtualMethod3() {}
+    virtual void someVirtualMethod4() {}
+    virtual void someVirtualMethod5() {}
+    virtual void someVirtualMethod6() {}
+    virtual void someVirtualMethod7() {}
+    virtual void someVirtualMethod8() {}
+    virtual void someVirtualMethod9() {}
+    virtual void someVirtualMethod10() {}
+    virtual void someVirtualMethod11() {}
+
+    std::string strMember = "";
+    int intMember = 0;
+    Something justSomething;
+};
+
+// !Complex class definition
 
 void benchmark_simple()
 {
@@ -12,8 +92,8 @@ void benchmark_simple()
 
 void benchmark_complex()
 {
-    std::vector<int> complex_vector;
-    std::list<int> complex_list;
+    std::vector<SomeComplexClass> complex_vector;
+    std::list<SomeComplexClass> complex_list;
 }
 
 int main()
