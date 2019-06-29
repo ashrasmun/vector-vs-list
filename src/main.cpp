8d93de632513f7c46cc42213d519a23366dc821c
ashrasmun <ashrasmun@gmail.com>
2019-06-29 22:38:12 +0200

    Adds simple benchmark for element insertion

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,14 @@ class SomeComplexClass;
 class Something
 {
 public:
-    Something(const SomeComplexClass&) {}
+    Something() {}
 
 private:
     std::string complexStr;
     std::map<
         std::pair<
             std::string,
-            std::chrono::duration<
-                std::chrono::nanoseconds
-                >
-            >,
+            int>,
         std::map<
             int,
             std::string
@@ -56,6 +53,7 @@ class SomeComplexClass
     friend class Something;
 
 public:
+    SomeComplexClass() {}
     virtual void someVirtualMethod() {}
 
 private:
@@ -84,16 +82,69 @@ private:
 
 // !Complex class definition
 
+template<typename Container>
+std::chrono::duration<double> benchmark_container_insertion(int number_of_elements_to_insert)
+{
+    using namespace std::chrono;
+
+    Container container;
+    typename Container::value_type element;
+    const auto beginning = steady_clock().now();
+
+    for (int i = 0; i < number_of_elements_to_insert; ++i)
+        container.push_back(element);
+
+    const auto after_insert = steady_clock().now();
+    return after_insert - beginning;
+}
+
+void printInsertionTime(const std::string& containerName, int N, std::chrono::duration<double> time)
+{
+    std::cout << containerName << " insertion time for " << N << " elements\n";
+    std::cout << time.count() << "\n\n";
+}
+
+template<typename UnderlyingType>
+void benchmark_insertion_into_containers(int n)
+{
+    const auto vector_insertion_duration = benchmark_container_insertion<std::vector<UnderlyingType>>(n);
+    printInsertionTime("Vector", n, vector_insertion_duration);
+
+    const auto list_insertion_duration = benchmark_container_insertion<std::list<UnderlyingType>>(n);
+    printInsertionTime("List", n, list_insertion_duration);
+}
+
+void printSurroundedWith(const std::string& message, char surroundingCharacter)
+{
+    for (size_t i = 0; i < message.size() + 3; ++i)
+        std::cout << surroundingCharacter;
+
+    std::cout << surroundingCharacter << '\n';
+
+    std::cout << surroundingCharacter << ' ' << message << ' ' << surroundingCharacter << '\n';
+
+    for (size_t i = 0; i < message.size() + 3; ++i)
+        std::cout << surroundingCharacter;
+
+    std::cout << surroundingCharacter << "\n\n";
+}
+
 void benchmark_simple()
 {
-    std::vector<int> simple_vector;
-    std::list<int> simple_list;
+    printSurroundedWith("Benchmarking insertion to containers for simple types (int)", '=');
+    benchmark_insertion_into_containers<int>(100);
+    benchmark_insertion_into_containers<int>(10000);
+    benchmark_insertion_into_containers<int>(1000000);
+    benchmark_insertion_into_containers<int>(100000000);
 }
 
 void benchmark_complex()
 {
-    std::vector<SomeComplexClass> complex_vector;
-    std::list<SomeComplexClass> complex_list;
+    printSurroundedWith("Benchmarking insertion to containers for complex types (SomeComplexClass)", '=');
+    benchmark_insertion_into_containers<SomeComplexClass>(100);
+    benchmark_insertion_into_containers<SomeComplexClass>(10000);
+    benchmark_insertion_into_containers<SomeComplexClass>(1000000);
+    benchmark_insertion_into_containers<SomeComplexClass>(100000000);
 }
 
 int main()
@@ -101,6 +152,5 @@ int main()
     benchmark_simple();
     benchmark_complex();
 
-    std::cout << "Just a test";
     return 0;
 }
